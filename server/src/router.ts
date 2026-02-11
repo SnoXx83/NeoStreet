9c058ed19d8a964aae818a0ce6d66637604c0dbc
baji <baji.lamallem@gmail.com>
2026-02-11 16:08:20 +0100

    create productRepository,productEntity,productController

--- server/src/router.ts
+++ server/src/router.ts
@@ -7,11 +7,11 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Define item-related routes
-import itemActions from "./modules/item/itemActions";
+import productController from "./modules/products/productController";
 
-router.get("/api/items", itemActions.browse);
-router.get("/api/items/:id", itemActions.read);
-router.post("/api/items", itemActions.add);
+router.get("/api/items", productController.browse);
+router.get("/api/items/:id", productController.read);
+router.post("/api/items", productController.add);
 
 /* ************************************************************************* */
 
