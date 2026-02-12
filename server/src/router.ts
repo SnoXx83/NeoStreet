52e03705a314d5f57fc4b057b27bbdd7e0f30b4f
baji <baji.lamallem@gmail.com>
2026-02-12 13:55:25 +0100

    create userRepository,userController,userEntity

--- server/src/router.ts
+++ server/src/router.ts
@@ -9,9 +9,14 @@ const router = express.Router();
 // Define item-related routes
 import productController from "./modules/products/productController";
 
-router.get("/api/items", productController.browse);
-router.get("/api/items/:id", productController.read);
-router.post("/api/items", productController.add);
+router.get("/api/products", productController.browse);
+router.get("/api/products/:id", productController.read);
+router.post("/api/products", productController.add);
+
+// router.get("/api/users", userController.browse);
+// router.get("/api/users/:id", userController.read);
+// router.post("/api/users", userController.add);
+
 
 /* ************************************************************************* */
 
