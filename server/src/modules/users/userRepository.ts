b5a7239ef69090726701f716aafaa330e0945d92
baji <baji.lamallem@gmail.com>
2026-02-12 09:52:43 +0100

    create userRepository,userEntity,userController

---
+++ server/src/modules/users/userRepository.ts
@@ -0,0 +1,63 @@
+import databaseClient from "../../../database/client";
+
+import type { Result, Rows } from "../../../database/client";
+
+import type { User } from "./userEntity";
+
+class UserRepository {
+  // The C of CRUD - Create operation
+
+  async create(user: Omit<User, "id">) {
+    // Execute the SQL INSERT query to add a new user to the "user" table
+    const [result] = await databaseClient.query<Result>(
+      "insert into user (first_name, last_name, email, password, logo_url) values (?, ?, ?, ?, ?)",
+      [
+        user.first_name,
+        user.last_name,
+        user.email,
+        user.password,
+        user.logo_url,
+      ],
+    );
+
+    // Return the ID of the newly inserted user
+    return result.insertId;
+  }
+
+  // The Rs of CRUD - Read operations
+
+  async read(id: number) {
+    // Execute the SQL SELECT query to retrieve a specific user by its ID
+    const [rows] = await databaseClient.query<Rows>(
+      "select * from user where id = ?",
+      [id],
+    );
+
+    // Return the first row of the result, which represents the user
+    return rows[0] as User;
+  }
+
+  async readAll() {
+    // Execute the SQL SELECT query to retrieve all users from the "user" table
+    const [rows] = await databaseClient.query<Rows>("select * from user");
+
+    // Return the array of users
+    return rows as User[];
+  }
+
+  // The U of CRUD - Update operation
+  // TODO: Implement the update operation to modify an existing user
+
+  // async update(user: user) {
+  //   ...
+  // }
+
+  // The D of CRUD - Delete operation
+  // TODO: Implement the delete operation to remove an user by its ID
+
+  // async delete(id: number) {
+  //   ...
+  // }
+}
+
+export default new UserRepository();
