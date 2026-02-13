3fcd0420ad75aad1e0cc70bbd3c349242e96ddca
maxime <maximelarquetoux@gmail.com>
2026-02-13 10:47:18 +0100

    feature: navbar added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import ProductDisplay from "./components/ProductDisplay";
-
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
       <main className="text-box">
+        <NavBar/>
         <div>
           <ProductDisplay />
         </div>
